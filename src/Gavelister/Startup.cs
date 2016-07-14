e3403d8eeb0c602f5b08786d9b851a9475c46455
eporkg <oyvindsbk@gmail.com>
2016-07-14 17:51:22 +0200

    Refactoring

--- src/Gavelister/Startup.cs
+++ src/Gavelister/Startup.cs
@@ -43,10 +43,6 @@ namespace Gavelister
                 .AddDefaultTokenProviders();
 
             services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
